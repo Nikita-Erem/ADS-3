c88b0f1c15a4e45347d9e5793726329b11b95dab
Nikita-Erem <160235350+Nikita-Erem@users.noreply.github.com>
2024-03-26 08:13:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int left = 0;
+    int right = size - 1;
+    int count = 0;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            int i = mid - 1;
+            while (i >= 0 && arr[i] == value) {
+                count++;
+                i--;
+            }
+            int j = mid + 1;
+            while (j < size && arr[j] == value) {
+                count++;
+                j++;
+            }
+            return count;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+  
   return 0; // если ничего не найдено
 }
